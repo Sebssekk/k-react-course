6db599238c5cc2fad61cf23d0cba006061fe2d59
sebssekk <sebssekk@hotmail.it>
2024-06-06 20:41:22 +0200

    Global Handlers

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { UsersList } from "./components/UsersList"
 export const App = () => {
     const isAuthenticated = false
 
+    window.addEventListener('resize', (e) => {
+        console.log(`X size: ${window.innerWidth}px\nY size: ${window.innerHeight}px`)
+    })
+
     return (  
     <>
         <UsersList isAuthenticated={isAuthenticated}/>
