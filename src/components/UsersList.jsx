f0d9e2a3e3405f2b653afe8efc07131f42f02ded
sebssekk <sebssekk@hotmail.it>
2024-06-06 14:32:19 +0200

    Multi condition rendering

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -29,7 +29,10 @@ export const UsersList = () => {
         
     ]
 
-    const isAuthenticated = false
+    const isAuthenticated = true
+    
+    // userType: ADMIN | COMMON | DISABLED
+    const userType = "COMMON"
 
     const renderedUsers = <div className="flex gap-5 justify-center">
         {users.map(u => (
@@ -37,10 +40,32 @@ export const UsersList = () => {
         ))}
     </div>
 
+    let userContent; 
+    switch (userType) {
+        case "ADMIN":
+            userContent = renderedUsers
+            break
+        case "COMMON":
+            userContent = <h1 className="text-blue-500 p-3 
+                                        font-semibold text-center 
+                                        border-blue-900 rounded-xl 
+                                        border-solid border-2 w-fit">
+                Welcome and nice to meet you !
+            </h1>
+            break
+        default:
+            userContent = <h1 className="text-gray-500 p-3
+                                font-semibold text-center
+                                border-gray-900 rounded-xl
+                                border-solid border-2 w-fit">
+                Sorry your account was disabled...
+            </h1>
+    }
+
     return <div>
         <section className="flex items-center justify-center flex-col">
             <h1 className="text-2xl font-bold text-center p-5">Current Users</h1>
-            {isAuthenticated ? renderedUsers : 
+            {isAuthenticated ? userContent : 
                 <h1 className="text-red-500 p-3 
                                 font-semibold text-center 
                                 border-red-900 rounded-xl 
