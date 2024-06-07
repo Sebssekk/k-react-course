7bfc88eb49f6217670483dcd2db1f6e2c6ff21dc
sebssekk <sebssekk@hotmail.it>
2024-06-07 10:06:28 +0200

    UseState with object

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react"
 
 export const LoginForm = () => {
-    const [usernameState, setUsername] = useState('')
-    const [passwordState, setPassword] = useState('')
+    //const [usernameState, setUsername] = useState('')
+    //const [passwordState, setPassword] = useState('')
+    const [loginState, setLogin] = useState({
+        username: '',
+        password: ''
+    })
 
     const handleChange = (changeEvent) => {
         console.log(`****
@@ -10,6 +14,10 @@ export const LoginForm = () => {
         Event trigger: ${changeEvent.currentTarget.id}
         Event value: ${changeEvent.currentTarget.value}
         ****`)
+        setLogin({
+            ...loginState,
+            [changeEvent.currentTarget.id]: changeEvent.currentTarget.value
+        })
     }
     const handleBlur = (blurEvent) => {
         console.log(`****
@@ -23,19 +31,17 @@ export const LoginForm = () => {
         console.log(`****
         -- SUBMIT EVENT --
         `)
-        console.log(`Username: ${usernameState}`)
-        console.log(`Password: ${passwordState}`)
+        console.log(loginState)
         // fetch('http://login-backend/api/login',{
         //     method: 'POST',
         //     headers: {
         //         'Content-Type': 'application/json'
         //     },
-        //     body: JSON.stringify({
-        //         username: usernameState,
-        //         password: passwordState
-        //     })
+        //     body: JSON.stringify(loginState)
         // })
     }
+
+    const formIsNotComplete = !loginState.username || !loginState.password
     
     return (
         <div className="flex flex-col items-center">
@@ -48,27 +54,27 @@ export const LoginForm = () => {
                 <label className="text-xs" htmlFor="username">Username</label><br />
                 <input className="rounded-lg px-1 font-thin italic bg-blue-950" 
                     type="text" id="username"
-                    //onChange={handleChange}
-                    onChange={(e) => {setUsername(e.currentTarget.value)}}
-                    value={usernameState}
+                    onChange={handleChange}
+                    //onChange={(e) => {setUsername(e.currentTarget.value)}}
+                    value={loginState.username}
                     onBlur={handleBlur}/>
             </div>
             <div>
                 <label className="text-xs" htmlFor="password">Password</label><br />
                 <input className="rounded-lg px-1 bg-blue-950" 
                     type="password" id="password"
-                    //onChange={handleChange}
-                    onChange={(e) => {setPassword(e.currentTarget.value)}}
-                    value={passwordState}
+                    onChange={handleChange}
+                    //onChange={(e) => {setPassword(e.currentTarget.value)}}
+                    value={loginState.password}
                     onBlur={handleBlur}/>
             </div>
             <br />
             <input className={`border-2 py-1 px-2 rounded-md 
-                            ${(!usernameState || !passwordState)?
+                            ${formIsNotComplete?
                                 "border-gray-950 bg-gray-600 text-gray-400":
                                 "border-blue-950 bg-blue-600 hover:bg-blue-400 hover:border-blue-500"} 
                             `}
-                    type="submit" disabled={!usernameState || !passwordState}
+                    type="submit" disabled={formIsNotComplete}
                     value="Login" />
             </form>
         </div>
